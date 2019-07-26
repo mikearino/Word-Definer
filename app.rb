e276d6420a5e54cc1935eee374f39ed91a03e730
Mike Lofgren <mikeylofgren@gmail.com>
2019-07-26 12:39:19 -0700

    work on edit words.

--- app.rb
+++ app.rb
@@ -25,4 +25,20 @@ post('/words') do
   @words = Word.all()
   erb(:words)
 end
+
 get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
+
+patch('/words/:id') do
+  @word = Words.find(params[:id].to_i())
+  @word.update(params[:name])
+  @words = Word.all
+  erb(:words)
+end
