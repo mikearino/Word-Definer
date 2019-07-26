d44fd59d04c11670a370681fc240efb2f14a0a93
Mike Lofgren <mikeylofgren@gmail.com>
2019-07-26 11:53:53 -0700

    add get/post for words.

--- app.rb
+++ app.rb
@@ -1,3 +1,27 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/word')
+require('pry')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_word)
+end
+
+post('/words') do
+  name = params[:word_name]
+  word = Word.new(name, nil)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
